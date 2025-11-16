5ad84517d850bd56d2c9e37a1ca1f5f9b083ce87
Noah Wood <26890744+NoahGWood@users.noreply.github.com>
2025-11-16 16:41:55 -0500

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,12 +4,12 @@
 int main() {
     std::string app = "TestApp";
 
-    std::cout << "App Dir:     " << std::filesystem::get_app_dir(app) << "\n";
-    std::cout << "Config Dir:  " << std::filesystem::get_config_dir(app) << "\n";
-    std::cout << "Cache Dir:   " << std::filesystem::get_cache_dir(app) << "\n";
-    std::cout << "Data Dir:    " << std::filesystem::get_data_dir(app) << "\n";
-    std::cout << "Log Dir:     " << std::filesystem::get_log_dir(app) << "\n";
-    std::cout << "Temp Dir:    " << std::filesystem::get_temp_dir() << "\n";
+    std::cout << "App Dir:     " << PlatformDirs::get_app_dir(app) << "\n";
+    std::cout << "Config Dir:  " << PlatformDirs::get_config_dir(app) << "\n";
+    std::cout << "Cache Dir:   " << PlatformDirs::get_cache_dir(app) << "\n";
+    std::cout << "Data Dir:    " << PlatformDirs::get_data_dir(app) << "\n";
+    std::cout << "Log Dir:     " << PlatformDirs::get_log_dir(app) << "\n";
+    std::cout << "Temp Dir:    " << PlatformDirs::get_temp_dir() << "\n";
 
     return 0;
 }
